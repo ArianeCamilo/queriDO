06d5330251cc024d790521526b9829ad24628ab0
User <ppkrauss@gmail.com>
2017-04-03 09:32:17 -0300

    adding funcs to mark2, and testing new docs with MKDOCS

---
+++ docs/report/curadoria001.md
@@ -0,0 +1,47 @@
+---
+title: Apresentação da Curadoria 001
+version: 1.0
+version_status: PRODUCTION
+endorsed_n: 2
+endorsed_status: approved
+layout: page
+---
+
+# Curadoria do Desabamento da ciclovia Tim Maia
+
+**Objetivo**: levantamento, no DOM do Rio de Janeiro, dos custos e responsabilidades pela construção da ciclovia Tim Maia, que desabou em abril de 2016.
+
+**Inicio**: 2016-07-31
+
+## Curadores
+
+* João Meirelles ([joaomeirelles](https://github.com/joaomeirelles))
+* Henrique Lorea ([henriquelorea](https://github.com/henriquelorea))
+* Bruna Santos ([membro da OKBR](https://br.okfn.org/membros/) e *zeladora da curadoria*)
+* Peter Krauss ([ppkrauss](https://github.com/ppKrauss) [membro da OKBR](https://br.okfn.org/membros/))
+* ... estamos aguardando a sua participação!  
+
+## Origem e motivações do projeto
+
+![](assets/curadoria001-desabamento.png)
+
+O [acidente da ciclovia Tim Maia, obra da Concremat](http://brasil.elpais.com/brasil/2016/04/21/politica/1461256688_847248.html) motivou um grupo de amigos a passar a noite em busca de informações sobre contratos da empreiteira com a cidade do Rio. O único caminho viável para executar esta tarefa hercúlea foi tentar extrair as informações do diário oficial da cidade. A tarefa os alertou para o quão valioso seria um script capaz de extrair sistematicamente informações do diário oficial da cidade e torná-las verdadeiramente abertas. As possibilidades a partir daí seriam inúmeras. Em excel, gráfico, verso e prosa.
+
+O Diário Oficial é como um blog importantíssimo  que poucos leem. No fomato e linguagem atuais, o Diário é "aberto" apenas por alguns usuários descontentes.
+Na tentativa de torná-lo aberto e reutilizável, não só no formato, mas na linguagem, iniciamos uma saga não só de programação, mas de idealização desse projeto que chamamos carinhosamente de *"nosso querido diário oficial"*. A empreitada chamou a atenção do Govlab em NY,  onde foi realizado um *coaching*, entre agosto e outubro de 2016.
+
+Entre agosto e setembro de 2016, Bruna Santos e Henrique Lorea, dois dos amigos que iniciaram a extração das informações do DO carioca com a [OKBr](http://br.okfn.org/). Organização da qual Bruna é membro. Em um dos encontro para planejamento estratégico em 2016, Peter ficou sabendo, e sugeriu unificar os requisitos do *QueriDO* com o [Diário Livre](http://devcolab.each.usp.br/do/), desenvolvido pelo COLAB-USP e apoiado pela OKBr, criando aqui no *git coletivo `okfn-brasil`* uma iniciativa mais ampla... Partes do código do Diário Livre,  como o [trazdia](https://github.com/andresmrm/trazdia) do Andres é semelhante ao que vinham desenvolvendo no *QueriDO*.
+
+No final de dezembro de 2016, com chegada de um voluntário animado, o Marco, decidimos criar um plano de metas para 2017: é o que está resumido na seção acima dos objetivos. 
+
+##  Levantamentos preliminares
+
+Foram pesquisadas todas as edições do Diário Oficial do Município (DOM) do Rio de Janeiro, e encontradas citações da empresa responsável pela construção da ciclovia, a **CONCREMAT, de 2012 a 2016**. Apenas separatas com alguma citação explícita foram requisitadas, para primeira análise e triagem.
+
+## Endossos e revisores desta página
+
+Além dos [revisores listados no histórico desta página](https://github.com/okfn-brasil/queriDO/commits/master/report/curadoria001.md), curdores que endossam o seu conteúdo:
+
+* Bruna Santos
+* Marco Zanchi
+* Você curador: edite o arquivo para acrescentar **seu nome aqui**, precisamos no mínimo 2 curadores **endossando** (atestando que concorda com todo o contedo acima) para iniciar o processo.
--- src/mark2.php
+++ src/mark2.php
@@ -28,7 +28,7 @@ $marcados = 'content/marcado'; 	// destino!
    echo "\n... using $f";
    $givenName_rgx[$ridx] = file_get_contents($f);
    $gnRegexAgg[$ridx] = '/(?<=[\s>])('. $givenName_rgx[$ridx] .')\s+_#gname_(\d+)#_/usi';
-   $gnRegex[$ridx]    = '#(?<=[\s>])(?:'. $givenName_rgx[$ridx] .')\s+(?:(?:\p{Lu}\p{Ll}+|d[oa]s?|de[lr]?|dal|e|van)\s+){0,8}(?:\p{Lu}\p{Ll}+)(?=[,;\.\(\)\[\]\s<])#us'; 
+   $gnRegex[$ridx]    = '#(?<=[\s>])(?:'. $givenName_rgx[$ridx] .')\s+(?:(?:\p{Lu}\p{Ll}+|d[oa]s?|de[lr]?|dal|e|van)\s+){0,8}(?:\p{Lu}\p{Ll}+)(?=[,;\.\(\)\[\]\s<])#us';
     // full name in free context, case sensitive.
    $gnRegex2[$ridx] = '#<b>((?:[^<]*?|\s+)?(?:'. $givenName_rgx[$ridx] .')\s[^<]+?)</b>#ius';  // clue for something into the bolds...
    $gnRegex3[$ridx] = '#(?<=[\s>,;]|^)(?:'. $givenName_rgx[$ridx ].')(?:\s+\p{L}+){0,8}\s+\p{L}+#ius';  // full name in bold context.
@@ -37,14 +37,15 @@ $marcados = 'content/marcado'; 	// destino!
 // END:PREPARE
 
 // see also (vocativos) at http://www.casacivil.pr.gov.br/modules/conteudo/conteudo.php?conteudo=9
-$honorificPrefix_rgx = 'acad[eê]mico|administrador|advogado|almirante|aluno|apost[oó]lico|ap[óo]stolo|arcebispo|Arquiteto|bacharela?|Bibliotec[aá]ri[ao]|bispo|cadete|capitão|cardeal|comandante|cirurgi[aã]o?\-dentista|cirurgi[aã]o?|comendador|cônego|conselheiro|contador|contra\-almirante|coronel|deputad[oa]|desembargadora?|di[áa]cono|Dign[íi]ssimo|Dom|Dona|doutora?|embaixadora?|Emin[êe]ncia|Eminentíssimo|enfermeiro|engenheir[oa]|enviado\s+extraordinário|plenipotenciário|Estado\-Maior|Excelentíssim[oa]|Excelência|general|ilustríssim[oa]|madame|mademoiselle|major|major\-brigadeiro|marechal|médico|Meritíssimo|mestr[ea]|mister|monsenhor|monsieur|Muit?o?\s+Digno|padre|pároco|pastora?|Philosophiae\s+Doctor|prefeito|presbítero|presidente|procurador|professora?|Profa?\.|promotora?|rei|Reverendíssim[ao]|Reverendo Padre|Reverendo|sacerdote|Santo Padre|sargento|sargento\-ajudante|secretári[oa]|senador|senhora?|sra?\.|senhorita|Sua\s+(?:Alteza\sReal|Alteza|Eminência|Excelência|Excelência\sReverendíssima|Majestade|Reverência|Reverendíssima|Santidade|Senhoria)|tenente\-coronel|tenente|tesoureiro|testemunha|vereador|veterinário|vice\-(?:almirante|presidente)|vig[aá]rio|viscondessa|visconde|Vossa\s(?:Alteza|Excelência|Eminência|Magnific[eê]ncia|Majestade|Senhoria)';
-$stopName_rgx = 'Avenida|Aeroporto|Área|Alameda|Campo|Comunidade|Condom[ií]nio|Ch[aá]cara|Distrito|Esta[çc][aã]o|Estrada|Pra[çc]a|Passeio|Rua|Travessa|Hospital|Funda[çc][aã]o|Monumento|Rodovia|T[úu]nel|Viaduto';
+$honorificPrefix_rgx = 'acad[eê]mico|administrador|advogado|almirante|aluno|apost[oó]lico|ap[óo]stolo|arcebispo|Arquiteto|bacharela?|Bibliotec[aá]ri[ao]|bispo|cadete|capitão|cardeal|comandante|cirurgi[aã]o?\-dentista|cirurgi[aã]o?|comendador|cônego|conselheiro|contador|contra\-almirante|coronel|deputad[oa]|desembargadora?|di[áa]cono|Dign[íi]ssimo|Dom|Dona|doutora?|embaixadora?|Emin[êe]ncia|Eminentíssimo|enfermeiro|engenheir[oa]|enviado\s+extraordinário|plenipotenciário|Estado\-Maior|Excelentíssim[oa]|Excelência|general|ilustríssim[oa]|madame|mademoiselle|major|major\-brigadeiro|marechal|médico|Meritíssimo|mestr[ea]|mister|monsenhor|monsieur|Muit?o?\s+Digno|padre|pároco|pastora?|Philosophiae\s+Doctor|prefeito|presbítero|presidente|procurador|professora?|Profa?\.|promotora?|rei|Reverendíssim[ao]|Reverendo Padre|Reverendo|sacerdote|Santo Padre|sargento|sargento\-ajudante|secretári[oa]|senador|senhora?|sra?\.|senhorita|Sua\s+(?:Alteza\sReal|Alteza|Eminência|Excelência|Excelência\sReverendíssima|Majestade|Reverência|Reverendíssima|Santidade|Senhoria)|tenente\-coronel|tenente|tesoureiro|testemunha|vereador|veterinário|vice\-(?:almirante|presidente)|vig[aá]rio|viscondessa|visconde|Vossa\s(?:Alteza|Excelência|Eminência|Magnific[eê]ncia|Majestade|Senhoria)|Senhora?|Sra?\.';
+$stopName_rgx = 'Avenida|São|Santa|Aeroporto|Área|Creche|Alameda|Campo|Comunidade|Condom[ií]nio|Ch[aá]cara|Distrito|Instituto|Esta[çc][aã]o|Estrada|Pra[çc]a|Passeio|Rua|Travessa|Hospital|Funda[çc][aã]o|Monumento|Rodovia|T[úu]nel|Viaduto';
+$localName_rgx = 'Sala\s+das\s+Sess[oõ]es|Sala|Monumento\s+ao|Monumento|Distrito\s+de|Creche(?:\s+Municipal|\s+Estadual|\s+Federal|)|Avenida|Aeroporto|Alameda|Comunidade|Condom[ií]nio|Ch[aá]cara|Distrito|Esta[çc][aã]o|Estrada|Pra[çc]a|Passeio\s+d[oa]s?|Parque|Rua|Travessa|Hospital|Funda[çc][aã]o|Rodovia|T[úu]nel|Viaduto';
 
 
 /* LEMBRETE: topônimos e cia, no parsing de segunda ordem,
      "Instituto <mark class="givenName">Fulano</mark>, rua <mark class="givenName">Ciclano</mark>".
      envelopar como Place ou NamedEntity
-     <span class="NamedEntity" data-type="instituto">Instituto <mark class="givenName">Fulano</mark></span>,
+     <span class="NamedEntity" data-type="organization">Instituto <mark class="givenName">Fulano</mark></span>,
      <span class="Place" data-type="via">rua <mark class="givenName">Ciclano</mark></span>.
      <span class="Place" data-type="via">avenida Marginal</span>.
 */
@@ -75,6 +76,7 @@ function mark($file) {
 	global $oldmarks;
 	global $honorificPrefix_rgx;
 	global $stopName_rgx;
+  global $localName_rgx;
 
 	$clean = file_get_contents($file);
 
@@ -86,12 +88,12 @@ function mark($file) {
 	);
 
 	if ($useGivenName) {
-	  $oldmarks = [];
-          $clean = preg_replace( // avoid toponimes
-                '#(?<=[\s>])(?:'. $stopName_rgx .')(?=[,;\.\(\)\[\]\s<])#usi',
-                '=stopName=$0=',
-                $clean
-          );
+    $oldmarks = [];
+    $clean = preg_replace( // avoid toponimes
+      '#(?<=[\s>])(?:'. $stopName_rgx .')(?=[,;\.\(\)\[\]\s<])#usi',
+      '=stopName=$0=',
+      $clean
+    );
 
 	  /*
 	  $clean = preg_replace_callback(   // (REDUNDANT NO EFECT HERE) remove and count marks
@@ -102,16 +104,16 @@ function mark($file) {
 	  */
 	  for($i=0;$i<$ridx; $i++) {  // each block of proper-name-regexes
 
-                $clean = preg_replace_callback(   // aggregates firstname to oldmark.
-                        $gnRegexAgg[$i],
-                        function ($m) {
-				global $oldmarks;
-				$oldmarks[] ="$m[1] ".$oldmarks[$m[2]];
-                            	$n = count($oldmarks)-1;
-                            	return "_#gname_$n#_";
-			},
-                        $clean
-                );
+      $clean = preg_replace_callback(   // aggregates firstname to oldmark.
+        $gnRegexAgg[$i],
+        function ($m) {
+          global $oldmarks;
+          $oldmarks[] ="$m[1] ".$oldmarks[$m[2]];
+          $n = count($oldmarks)-1;
+          return "_#gname_$n#_";
+        },
+        $clean
+      );
 		$clean = preg_replace_callback(   // marca em texto livre.
 			$gnRegex[$i], 'oldmark_enfold0', $clean
 		);
@@ -126,7 +128,7 @@ function mark($file) {
 
 	  } // for
 
-          $clean = preg_replace( // add prefixes
+          $clean = preg_replace( // add prefixes when names
                 "/($honorificPrefix_rgx)\s+(_#gname_(\d+)#_)/usi",
                 '<span class="honorificPrefix">$1</span> $2',
                 $clean
@@ -144,33 +146,45 @@ function mark($file) {
 
 	} // if
 
+ if (0) {  // check nomes de locais (mesmo sendo nome de gente)
+   $oldmarks = [];
+   $clean = preg_replace_callback(   // marca em texto livre.
+     '|<span class="Person">|', 'oldmark_enfold0', $clean
+   );
 
+   $clean = preg_replace( // avoid toponimes
+     '#(?<=[\s>])(?:'. $localName_rgx .')(?=[,;\.\(\)\[\]\s<])#usi',
+     '=localName=$0=',
+     $clean
+   );
+
+}
 	// // // //
 	// quase homologados, marcações atômicas, de primeira ordem:
 
 	$clean = preg_replace('#(?:CNPJ[\s:\-\.]*)?\d\d\.\d\d\d\.\d\d\d/\d\d\d\d\-\d\d#uis', '<data class="urn-org-vatID">$0</data>', $clean);
 
-	//(nao usar .+) $n1 = 0; $clean = preg_replace('#Artigo\s+[\d\.]+\sInciso\s+.+?\s+da\s+Lei\s+n?º?\s*\d[\d\.]+\d[/\s]+\d\d\d\d#uis', 
+	//(nao usar .+) $n1 = 0; $clean = preg_replace('#Artigo\s+[\d\.]+\sInciso\s+.+?\s+da\s+Lei\s+n?º?\s*\d[\d\.]+\d[/\s]+\d\d\d\d#uis',
 	//	'<cite class="urn-lex">$0</cite>', $clean, -1, $n1);
 	$n = 0; $clean = preg_replace(
-		'#Artigo\s+[\d\.]+\sda\s+Lei\s+n?º?\s*\d[\d\.]+\d[/\s]+\d\d\d\d#uis', 
+		'#Artigo\s+[\d\.]+\sda\s+Lei\s+n?º?\s*\d[\d\.]+\d[/\s]+\d\d\d\d#uis',
 		'<cite class="urn-lex">$0</cite>', $clean,-1, $n
 	);
 	if (!$n) $clean = preg_replace(
-		'#(?:Lei|Descreto\s+Lei|Decreto|Portaria)\s+n?º?\s*\d[\d\.]+\d[/\s]+\d\d(\d\d)?#uis', 
-		'<cite class="urn-lex">$0</cite>', 
+		'#(?:Lei|Descreto\s+Lei|Decreto|Portaria)\s+n?º?\s*\d[\d\.]+\d[/\s]+\d\d(\d\d)?#uis',
+		'<cite class="urn-lex">$0</cite>',
 		$clean
 	  );
 
 	$clean = preg_replace('#Contrato\s+(?:N\.?º?\s*)?\d[\d\-\./]+(/\d\d\d\d)?#uis', '<cite class="urn-cntrt">$0</cite>', $clean);
 	$clean = preg_replace(
-	  '#Processo(?:\s+INSTRUTIVO|\s+administrativo)?\s*(?:N\.?º?\s*|:\s*)?\d[\d\-\./]+#uis', 
+	  '#Processo(?:\s+INSTRUTIVO|\s+administrativo)?\s*(?:N\.?º?\s*|:\s*)?\d[\d\-\./]+#uis',
 	  '<cite class="urn-gov-proc">$0</cite>',
 	  $clean
 	);
 	$clean = preg_replace(
-	  '#matr(?:\.|[ií]cula)\s*(?:N\.?º?\s*)?\d[\d\-\./]+#uis', 
-	  '<cite class="urn-gov-profreg">$0</cite>', 
+	  '#matr(?:\.|[ií]cula)\s*(?:N\.?º?\s*)?\d[\d\-\./]+#uis',
+	  '<cite class="urn-gov-profreg">$0</cite>',
 	  $clean
 	); // ProfessionalService Registration ID
 
@@ -178,7 +192,7 @@ function mark($file) {
 	// Restringindo (flag $n) estilo do documento para evitar vazamentos da regex:
 	$n = 0;
 	$clean = preg_replace(
-		'#(?:\s*<[ubi]>\s*)(?:\s*<[ubi]>\s*)Valor(?:\s*</[ubi]>\s*)(?:\s*</[ubi]>\s*)[\s:]*(de\s)?R\$\s*\d[\d\,\.]+#uis', 
+		'#(?:\s*<[ubi]>\s*)(?:\s*<[ubi]>\s*)Valor(?:\s*</[ubi]>\s*)(?:\s*</[ubi]>\s*)[\s:]*(de\s)?R\$\s*\d[\d\,\.]+#uis',
 		'<data class="currencyValue">$0</data>',
 		$clean,
 		-1,
@@ -194,7 +208,7 @@ function mark($file) {
 		$clean = preg_replace('#R\$\s*\d[\d\,\.]+#uis', '<data class="currencyValue">$0</data>', $clean);
 
 	$clean = preg_replace(
-	  '#\d\d?\s+de\s+(?:janeiro|fevereiro|mar[çc]o|abril|maio|junho|julho|agosto|setembro|outubro|novembro|dezembro)\s+de\s+\d\d\d\d#uis', 
+	  '#\d\d?\s+de\s+(?:janeiro|fevereiro|mar[çc]o|abril|maio|junho|julho|agosto|setembro|outubro|novembro|dezembro)\s+de\s+\d\d\d\d#uis',
 	  '<time class="date">$0</time>',
 	  $clean
 	);
